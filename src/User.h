ebcf12e6cfef11f9d3b02ccac3ce7844176c21b8
Andrii Korzhovskyi <gekotrex56@gmail.com>
2023-12-02 15:43:44 +0200

    Changes

---
+++ src/User.h
@@ -0,0 +1,18 @@
+
+using namespace std;
+
+class User {
+private:
+	string name;
+	string surname;
+
+public:
+	void say();
+
+	User(string name, string surname);
+
+	void setName(string name);
+	string getName();
+	void setSurname(string name);
+	string getSurname();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
+#include "User.h"
 using namespace std;
 
+int main() {
+
+	User user("John", "John");
+	user.say();
+	cout << endl;
+	cout << user.getName() << "\n";
+	cout << user.getSurname();
+	return 0;
+
+}
 
 
