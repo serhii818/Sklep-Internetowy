4e38733f43d842ea4724d5459fd5596b9de16ced
Andrii Korzhovskyi <gekotrex56@gmail.com>
2024-01-02 19:44:49 +0200

    ...

--- src/Sklep-Internetowy/Consumer.cpp
+++ src/Sklep-Internetowy/Consumer.cpp
@@ -6,7 +6,7 @@ Consumer::Consumer(string username, string password, string address, int phone, 
 void Consumer::displayInfo() 
 {
     User::displayInfo();
-    cout << "Username: " << address << endl;
+    cout << "Username: " << username << endl;
     cout << "Password: " << password << endl;
     cout << "Address: " << address << endl;
     cout << "Phone: " << phone << endl;
